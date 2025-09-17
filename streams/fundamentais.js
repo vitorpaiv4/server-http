2a19a55b28939bdbd1d4deb43a2a9b9fb2a48440
Vitor Gabriel Souza Pinheiro Paiva <vitor.paiva@emdurportovelho.com>
2025-09-17 09:29:09 -0300

    inversenumber incompleto

---
+++ streams/fundamentais.js
@@ -0,0 +1,29 @@
+
+
+//process.stdin
+//    .pipe(process.stdout);
+
+import {Readable, Writable} from 'node:stream';
+class OneToHunderedStream extends Readable {
+    index = 1
+    _read(){
+        const i = this.index++;
+setTimeout(() => {
+        if (i> 100){
+            this.push(null)
+        }else {
+
+            const buf = Buffer.from(String(i));
+            this.push(buf)
+      
+        }
+    }, 1000)
+    }}
+
+    class MultiplyByTenStream extends Writable{
+    _write(chunk, encoding, callback){
+    console.log(Number(chunk.toString()) * 10 );
+    callback();
+    }}
+    new OneToHunderedStream()
+    .pipe(new MultiplyByTenStream());
